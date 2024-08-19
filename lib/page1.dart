0e1c2bd1cf1f7125a471c4898c47c78e52cb85ee
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-19 12:26:39 +0300

    UPD 1

--- lib/page1.dart
+++ lib/page1.dart
@@ -7,30 +7,44 @@ class Page1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile', style: TextStyle(color: Colors.white)),
-        backgroundColor: const Color.fromARGB(255, 156, 159, 161),
+        title: const Text('Profile',
+            style: TextStyle(color: Color.fromARGB(255, 85, 82, 82))),
+        backgroundColor: const Color.fromARGB(255, 214, 211, 203),
       ),
       body: Column(
         children: [
+          const SizedBox(height: 50),
           CircleAvatar(
             radius: 80.0,
             child: Image.network(
                 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
           ),
-          Container(
-            height: 100,
-            width: 500,
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 156, 159, 161),
-            ),
-            child: const Text(
-              'Profile',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          const SizedBox(height: 50),
+          DefaultTabController(
+            length: 2,
+            child: Container(
+                height: 450,
+                width: 500,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 214, 211, 203),
+                      width: 5),
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  color: const Color.fromARGB(255, 231, 234, 227),
+                ),
+                child: const Column(
+                  children: [
+                    TabBar(tabs: [
+                      Tab(
+                        text: 'Profile',
+                      ),
+                      Tab(
+                        text: 'Edit Profile',
+                      ),
+                    ]),
+                  ],
+                )),
           ),
         ],
       ),
